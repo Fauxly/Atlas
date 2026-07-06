2e65e176ac99dea37b6f37b90259a9220be662ab
Your Name <tailormoon@icloud.com>
2026-07-06 11:17:05 +0300

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Fix’s Trick’s on 06.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtlasTests/AtlasTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasTests.swift
+//  AtlasTests
+//
+//  Created by Fix’s Trick’s on 06.07.2026.
+//
+
+import Testing
+@testable import Atlas
+
+struct AtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
